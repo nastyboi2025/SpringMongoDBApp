da9d26acba2132885199580367f526ba08eb73f3
Naser Ahmed <naser.ahmed@heg.com>
2016-02-05 13:12:30 +0000

    Second commit

---
+++ src/main/java/com/naser/mongodb/example/MongoDBApp.java
@@ -0,0 +1,39 @@
+package com.naser.mongodb.example;
+
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
+import org.springframework.context.ConfigurableApplicationContext;
+import org.springframework.context.support.ClassPathXmlApplicationContext;
+
+/**
+ * Small MongoDB application that uses spring data to interact with MongoDB.
+ * 
+ * @author Naser Ahmed
+ */
+public class MongoDBApp {
+
+    static final Logger logger = LoggerFactory.getLogger(MongoDBApp.class);
+
+    public static void main( String[] args ) {
+		logger.info("Bootstrapping MongoDemo application");
+
+		ConfigurableApplicationContext context = new ClassPathXmlApplicationContext("META-INF/spring/applicationContext.xml");
+
+        PersonRepository personRepository = context.getBean(PersonRepository.class);
+
+        // cleanup person collection before insertion
+        personRepository.dropPersonCollection();
+
+        //create person collection
+        personRepository.createPersonCollection();
+
+        for(int i=0; i<20; i++) {
+            personRepository.insertPersonWithNameJohnAndRandomAge();
+        }
+
+        personRepository.logAllPersons();
+        logger.info("Avarage age of a person is: {}", personRepository.getAvarageAgeOfPerson());
+
+        logger.info("Finished MongoDemo application");
+	}
+}
